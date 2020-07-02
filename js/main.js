2f7b25c4be2df7ce3f47dcd4c4f28e14e60c1f2c
Kareszka <karolymolnarat@gmail.com>
2020-07-02 18:27:57 +0200

    ujabbak

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 function calcAmount () {
-    let price = 1000;
-    let amountInput = document.querySelector("input[name='amount-input']");
+let price = 1000;
+let amountInput = document.querySelector("input[name='amount-input']");
+
+let amountNumber = parseInt(amountInput.value);
+amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+
+showSumPrice(price, amountNumber);
+}
+
+
+function showSumPrice(price, amountNumber) {
     let showAmount = document.querySelector("span.show-amount");
-    let amountNumber = parseInt(amountInput.value);
-    
-    if (amountNumber>10) {
+    if ( amountNumber > 10 ) {
         alert("Maximum 10 terméket vásárolhat!");
-        return;
     
-        } else if ( amountNumber < 1 ) {
+    } else if ( amountNumber < 1 ) {
         alert("Minimum 1 terméket vásárolni kell!");
         
-    }else {  
-        let amount = parseInt(amountInput.value) * price;  
+    } else {  
+        let amount = amountNumber * price;  
         showAmount.innerHTML = amount;
-        }
-    
-        
     }
+}
