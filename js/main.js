e6819aa17b04f96d7a171e272d77f36f5f0aa554
Kareszka <karolymolnarat@gmail.com>
2020-07-02 11:58:00 +0200

    mainjs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 function calcAmount () {
-let price = 1000;
-let amountInput = document.querySelector("input[name='amount-input']");
-let showAmount = document.querySelector("span.show-amount");
-let amount = parseInt(amountInput.value) * price;
-
-showAmount.innerHTML = amount;
-}
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = parseInt(amountInput.value);
+    
+    if (amountNumber>10) {
+        alert("Maximum 10 terméket vásárolhat!");
+        return;
+    
+        } else if ( amountNumber < 1 ) {
+        alert("Minimum 1 terméket vásárolni kell!");
+        
+    }else {  
+        let amount = parseInt(amountInput.value) * price;  
+        showAmount.innerHTML = amount;
+        }
+    
+        
+    }
